f85c116a80d3f3202613d069a8a45dd66fbbf0d6
Balthazar Rouberol <brouberol@askin.fr>
2015-06-01 09:53:36 +0200

    Read secret key from environment variable!
    You should NEVER publish any secret key on a public repo, that's just
    an easy way to get bitten.
    Have a look at this
    [article](http://www.itnews.com.au/News/375785,aws-urges-developers-to-scrub-github-of-secret-keys.aspx)
    for example.
    
    You should use environment variables (or any other method you like)
    for any sensitive information, and never make them public. Ever.

--- i2o/settings.py
+++ i2o/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j)844^e97^fn6m#iii1)w_&72m#q)1i3gv4md$u^7#8sscxj$^'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
